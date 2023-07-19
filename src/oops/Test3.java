fd7781c0a24e331b634865cdd46f79f800c8d65f
automationcatalogue <automation.catalogue@gmail.com>
2023-07-19 14:01:58 +0530

    Day9-Java Methods, this keyword

---
+++ src/oops/Test3.java
@@ -0,0 +1,18 @@
+package oops;
+
+public class Test3 {
+    //instance method -method definition
+    //method with return type and with arguments
+    static int max(int a, int b){
+        if(a>b){
+            return a;
+        }else{
+            return b;
+        }
+    }
+
+    public static void main(String[] args) {
+        Test3.max(10,4);
+
+    }
+}
