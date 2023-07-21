f27d151b93237376fcc3965ec8c168dad063b9f3
automationcatalogue <automation.catalogue@gmail.com>
2023-07-21 17:02:33 +0530

    Day10- Java OOPS Constructors and Inheritance

---
+++ src/oops/constructors/Demo.java
@@ -0,0 +1,29 @@
+package oops.constructors;
+
+import java.util.Scanner;
+
+public class Demo {
+
+    int z=20;
+
+    Demo(){//default constructor
+        this(8);
+        int x=10;
+        if(x%2==0){
+            System.out.println("Given number is Even!!!");
+        }else{
+            System.out.println("Given number is Odd!!!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Demo ob1 = new Demo();
+        Scanner ob = new Scanner(System.in);
+    }
+
+    Demo(int x){//Parameterized constructor
+        int y = x*x*x;
+        System.out.println("Cube of the given number is :"+y);
+    }
+
+}
---
+++ src/oops/constructors/Emp2.java
@@ -0,0 +1,11 @@
+package oops.constructors;
+
+public class Emp2 {
+    int empNo;
+    float empSalary;
+
+    void print(int x, float y){
+        this.empNo=x;
+        this.empSalary=y;
+    }
+}
