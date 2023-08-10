7aa011bf82dbf6d950afa178215f495be7d3db55
automationcatalogue <automation.catalogue@gmail.com>
2023-08-10 14:00:48 +0530

    Day19-String Handling and Wrapper Classes

---
+++ src/StringHandling/Demo19.java
@@ -0,0 +1,10 @@
+package StringHandling;
+
+public class Demo19 {
+    public static void main(String[] args) {
+        StringBuffer sb1 = new StringBuffer("Hello India");
+        System.out.println(sb1);
+
+        String s1 = new String(sb1);
+    }
+}
