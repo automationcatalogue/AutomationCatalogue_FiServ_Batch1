65960ff5f8f038b5ddef290905c028dd7a58edd4
automationcatalogue <automation.catalogue@gmail.com>
2023-08-18 14:11:37 +0530

    Day20, Wrapper Classes and Exception Handling

---
+++ src/exceptionHandling/Demo1.java
@@ -0,0 +1,19 @@
+package exceptionHandling;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        FileInputStream ob;
+        try{
+            ob = new FileInputStream("C:\\Users\\AaradhyaVashisht\\Desktop\\abcd.pdf");
+            System.out.println("File is opened in a read mode");
+        }catch(FileNotFoundException ob1){
+            System.out.println("File is not present in the specified location");
+        }finally {
+
+        }
+
+    }
+}
---
+++ src/wrapperClasses/Demo4.java
@@ -0,0 +1,14 @@
+package wrapperClasses;
+
+public class Demo4 {
+    public static void main(String[] args) {
+        String str="Order Number placed :TZ3530";
+        String str1[]=str.split(":");
+
+        System.out.println(str1[1]);
+
+        int x=Integer.parseInt(str1[1]);
+        x=x+100;
+        System.out.println(x);
+    }
+}
