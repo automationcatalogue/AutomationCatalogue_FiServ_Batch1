b8c2cf965de049bdf73ba9a9dfdb44a3bba21288
automationcatalogue <automation.catalogue@gmail.com>
2023-09-13 14:10:20 +0530

    Java Streams API

---
+++ src/java8Features/collection/Demo1.java
@@ -0,0 +1,21 @@
+package java8Features.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        ArrayList<Employee> al = new ArrayList<Employee>();
+        al.add(new Employee("Sagar",42342));
+        al.add(new Employee("Deepanshu",10423));
+        al.add(new Employee("Sumith",64533));
+        al.add(new Employee("Dharanishankar",53432));
+        System.out.println(al);
+
+       Comparator<Employee> c = (e1, e2)->-e1.empName.compareTo(e2.empName);
+       Collections.sort(al,c);
+       System.out.println(al);
+
+    }
+}
---
+++ src/java8Features/javaStreams/Demo1.java
@@ -0,0 +1,38 @@
+package java8Features.javaStreams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(0);
+        al.add(35);
+        al.add(20);
+        al.add(5);
+        al.add(15);
+        al.add(25);
+        System.out.println(al);
+
+        List<Integer> al3 = al.stream().filter(i->i%2==0).collect(Collectors.toList());
+        System.out.println(al3);
+
+        List<Integer> al4 = al.stream().map(i->i*2).collect(Collectors.toList());
+        System.out.println(al4);
+
+        long oddCount = al.stream().filter(i->i%2!=0).count();
+        System.out.println("Odd Numbers count in the ArrayList is :"+oddCount);
+
+        List<Integer> al5 = al.stream().sorted().collect(Collectors.toList());
+        System.out.println(al5);
+
+        List<Integer> al6 = al.stream().sorted((ob1, ob2) ->(ob1>ob2)?-1:(ob1<ob2)?1:0).collect(Collectors.toList());
+        System.out.println(al6);
+
+        al.stream().sorted().max((ob1,ob2)-> ob1>ob2?-1:1);
+
+
+    }
+}
